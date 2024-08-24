18e43f13c8aeecf5b1a9c510423fd6dfc7398e33
Rashodxon <rashodxon56@gmail.com>
2024-08-24 13:56:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+// let box = 'Rashodbek';
+// box = 'Muhammadmuso';
+
+// let ism = 'Jasurbek';
+// console.log(box);
+// console.log(ism);
+
+let ism = prompt("Ismingizni kiriting!");
+let familiya = prompt("familiyangizni kiriting!");
+let Yoshingizni  = prompt("Yoshingizni kirting!");
+let faningizni = prompt("Yoqtirgan faningizni yozing!");
+let tumanningizni = prompt("Yashaydigan tumanningizni kirting!");
+
+
+console.log("Ism: " + ism);
+console.log("Famimiliya: " + familiya);
+console.log("Yoshingizni kirting!: " + Yoshingizni);
+console.log("Yoqtirgan faningizni yozing!: " + faningizni);
+console.log("Yashaydigan tumanningizni kirting!: " + tumanningizni);
